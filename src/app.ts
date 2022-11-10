e3c549a3a69ab17c4e75d77abeb7c97e4f531295
shige <shige@asiainfo.com>
2022-11-10 18:37:06 +0800

    feat: add user router & post methods

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,16 @@ import Koa from 'koa'
 import body from 'koa-body'
 import json from 'koa-json'
 import Router from 'koa-router'
+import userRouter from './router/user'
 
 const app = new Koa()
 const router = new Router()
 // 所有的路由访问添加一级路由dang
 router.prefix('/dang')
-
-router.get('/test', async (ctx: Koa.Context, next: Koa.Next) => {
-  ctx.body = 'first test page'
-})
 router.use(json())
 router.use(body())
+
+router.use(userRouter.routes(), userRouter.allowedMethods())
 
 // 路由加载到全局路由上面
 app.use(router.routes())
